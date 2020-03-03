b2fb86867957e761389c6e1e85dd2d1719829fca
Stephen Sanchez <stephensanchez05@gmail.com>
2020-03-03 02:58:37 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,5 +4,14 @@ class Pirate
   
   @@all = []
   
-  def initialize()
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
